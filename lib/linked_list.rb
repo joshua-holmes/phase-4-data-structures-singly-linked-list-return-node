31a42d9621bdddfe2d9164127cc99c557f1f33f4
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-14 18:06:04 -0600

    commit tree d13df71a3c1e3e5729905b99518d9e5bdb179359

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,5 @@
 require_relative './node'
-
+require 'pry'
 class LinkedList
   attr_accessor :head
 
@@ -8,7 +8,24 @@ class LinkedList
   end
 
   def nth_from_end(n)
-    # your code here
+    i = get_length
+    return nil if i < n || n < 1
+    current_node = head
+    while i > n
+      current_node = current_node.next_node
+      i -= 1
+    end
+    current_node.value
+  end
+
+  def get_length
+    current_node = head
+    count = 0
+    while current_node do
+      count += 1
+      current_node = current_node.next_node
+    end
+    count
   end
 
 end
